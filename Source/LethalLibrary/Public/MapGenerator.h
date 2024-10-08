b11d16cfff171f666972c22965f090f9042e8322
Seth Grinstead <seth.grinstead3299@gmail.com>
2024-10-08 14:55:24 -0700

    Map Generator Component
    Creates the map generator c++ actor component to be attached to a gamemode

---
+++ Source/LethalLibrary/Public/MapGenerator.h
@@ -0,0 +1,31 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "MapGenerator.generated.h"
+
+
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class LETHALLIBRARY_API UMapGenerator : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:	
+	// Sets default values for this component's properties
+	UMapGenerator();
+
+	UFUNCTION(BlueprintCallable)
+	FString TestFunction();
+
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+
+		
+};
