59b9fd3a884eaf4b00dc0cb27e9ddb0bb50e2c97
Seth Grinstead <seth.grinstead3299@gmail.com>
2024-10-15 13:47:09 -0700

    Tile cpp files

---
+++ Source/LethalLibrary/Public/Tile.h
@@ -0,0 +1,23 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+/**
+ * 
+ */
+class LETHALLIBRARY_API Tile
+{
+public:
+	FString Shorthand;
+	int North;
+	int East;
+	int South;
+	int West;
+
+	Tile();
+	~Tile();
+
+	void UpdateShorthand();
+};
